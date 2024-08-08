39554763435c5a6608cc1dd9117e7fb9fc653447
igordon215 <igordon215@gmail.com>
2024-08-07 21:24:50 -0400

    removedupsexact

--- src/main/java/com/zipcodewilmington/looplabs/StringDuplicateDeleter.java
+++ src/main/java/com/zipcodewilmington/looplabs/StringDuplicateDeleter.java
@@ -1,5 +1,7 @@
 package com.zipcodewilmington.looplabs;
 
+import java.util.Arrays;
+
 /**
  * Created by leon on 1/28/18.
  * @ATTENTION_TO_STUDENTS You are forbidden from modifying the signature of this class.
@@ -16,6 +18,27 @@ public final class StringDuplicateDeleter extends DuplicateDeleter<String> {
 
     @Override
     public String[] removeDuplicatesExactly(int exactNumberOfDuplications) {
-        return new String[0];
+        String[] newArr = new String[0];
+
+        for (String x : array){
+            if(getNumberOfOccurrencesString(x) != exactNumberOfDuplications){
+                int index = newArr.length;
+                newArr = Arrays.copyOf(newArr,index+1);
+                newArr[index] = x;
+            }
+        }
+        return newArr;
+
     }
+
+    public int getNumberOfOccurrencesString(String valueToEvaluate) {
+        int counter = 0;
+        for (String element : array){
+            if (element.equals(valueToEvaluate)){
+                counter++;
+            }
+        }
+        return counter;
+    }
+
 }
